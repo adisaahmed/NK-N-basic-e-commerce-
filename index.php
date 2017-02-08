fac80ed957685ea20f7bf54acb94244037320acf
Ahmed Adisa <adisa.ahmed881@gmail.com>
2017-02-08 14:52:59 +0100

    i have converted the header to php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>NK&N | Home</title>
+		<link rel="stylesheet" href="css/ahmed.css">
+		<link rel="stylesheet" href="css/font/font.css">
+		<link rel="stylesheet" href="css/header.css">
+		<link rel="stylesheet" href="css/animate.css">
+		<link rel="stylesheet" href="css/hover.css">
+		<link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/imagehover.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		<meta charset="utf-8">
+	  	<meta name="viewport" content="width=device-width, initial-scale=1">
+	  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<?php include_once("header.php");?>
+	</body>
+</html>
+
